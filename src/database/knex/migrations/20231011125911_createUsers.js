cc1d7f87a8fc65034f8ad0a2bcd90239793bb948
Lucas Andrade <contato.developerlucas@gmail.com>
2024-02-06 15:08:03 -0300

    new deploy

--- src/database/knex/migrations/20231011125911_createUsers.js
+++ src/database/knex/migrations/20231011125911_createUsers.js
@@ -3,8 +3,8 @@ exports.up = knex => knex.schema.createTable("users", table => {
     table.text("name").notNullable();
     table.text("email").notNullable();
     table.text("password").notNullable();
-  
-    table.enum("role", ["admin", "customer"], {useNative: true, enumName: "roles"}).notNullable().default("admin")
+
+    table.enum("role", ["admin", "customer"], {useNative: true, enumName: "roles"}).default("customer")
   
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
